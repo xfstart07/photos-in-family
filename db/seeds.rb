65c7431442a17a9b428d6d0cd2de1db3e2618aba
Leon Xu <xfstart07@gmail.com>
2017-08-25 00:39:17 +0800

    数据库

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Tag.create(name: "魅族")
+Tag.create(name: '魅族')
